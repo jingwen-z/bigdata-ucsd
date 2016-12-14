54ce89f363c9efa9e2c5f7e5e2265cb25d7c7aa0
jingwen <jingwen7.zheng@gmail.com>
2016-12-14 23:41:19 +0100

    hands-on: data exploration in spark

---
+++ modeling-and-management-systems/data-exploration-in-spark.py
@@ -0,0 +1,29 @@
+"""
+Week 2 - Data Exploration
+
+Hands-on: Data Exploration in Spark
+"""
+
+from pyspark.sql import SQLContext
+
+# create an SQLContext
+sqlContext = SQLContext(sc)
+
+df = sqlContext.read.load('file:///home/cloudera/Downloads/big-data-4/daily_weather.csv',
+                          format = 'com.databricks.spark.csv',
+                          header = 'true',
+                          inferSchema = 'true')
+
+df.columns
+df.printSchema()
+
+df.describe().toPandas().transpose()
+df.describe('air_pressure_9am').show()
+
+len(df.columns)
+df.count()
+
+df2 = df.na.drop(subset = ['air_pressure_9am'])
+df2.count()
+
+df2.stat.corr("rain_accumulation_9am", "rain_duration_9am")
