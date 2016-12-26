ff24c1644964460f8fe896e317a6d42546e827a4
Jingwen Zheng <jingwen7.zheng@gmail.com>
2016-12-26 19:21:38 +0100

    week 5: hands-on exploring sparksql and spark data frames

---
+++ integration-and-processing/exploring-sparksql-and-spark-dataframes.py
@@ -0,0 +1,44 @@
+"""
+Week 5 - Big Data analytics using Spark
+
+Hands-on - Exploring SparkSQL and Spark DataFrames
+"""
+
+from pyspark.sql import SQLContext
+
+sqlsc = SQLContext(sc)
+
+# format("jdbc") says that the source of the DataFrame will be using a Java 
+# database connection
+# url option is the URL connection string to access the Postgres database
+# dbtable option specifies the gameclicks table
+
+df = sqlsc.read.format("jdbc") \
+    .option("url", "jdbc:postgresql://localhost/cloudera?user=cloudera") \
+    .option("dbtable", "gameclicks") \
+    .load()
+
+df.printSchema()
+df.count()
+df.show(5)
+
+df.select("userid", "teamlevel").show(5)
+
+df.filter(df["teamlevel"] > 1).select("userid", "teamlevel").show(5)
+
+df.groupBy("ishit").count().show()
+
+from pyspark.sql.functions import *
+df.select(mean('ishit'), sum('ishit')).show()
+
+df2 = sqlsc.read.format("jdbc") \
+    .option("url", "jdbc:postgresql://localhost/cloudera?user=cloudera") \
+    .option("dbtable", "adclicks") \
+    .load()
+
+df2.printSchema()
+
+merge = df.join(df2, 'userid')
+merge.printSchema()
+
+merge.show(5)
