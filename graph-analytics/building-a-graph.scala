f3f9689014714c8f12b59b7b4dbe43e6f4621c53
Jingwen Zheng <jingwen7.zheng@gmail.com>
2017-01-10 22:46:20 +0100

    update(+) comments

--- graph-analytics/building-a-graph.scala
+++ graph-analytics/building-a-graph.scala
@@ -2,27 +2,34 @@
 //
 // Hands On: Building A Graph
 
-// import the GraphX libraries
+/** import the GraphX libraries */
+
+// set log level to error, suppress info and warn messages
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
 Logger.getLogger("org").setLevel(Level.ERROR)
 Logger.getLogger("akka").setLevel(Level.ERROR)
 
+// Import the Spark's GraphX and RDD libraries along with Scala's source library
 import org.apache.spark.graphx._
 import org.apache.spark.rdd._
 
 import scala.io.Source
 
-// import the vertices
+/** import the vertices */
 Source.fromFile("./EOADATA/metro.csv").getLines().take(5).foreach(println)
 Source.fromFile("./EOADATA/country.csv").getLines().take(5).foreach(println)
 Source.fromFile("./EOADATA/metro_country.csv").getLines().take(5).foreach(println)
 
+// create case classes for the places (metros and countries)
 class PlaceNode(val name: String) extends Serializable
 case class Metro(override val name: String, population: Int) extends PlaceNode(name)
 case class Country(override val name: String) extends PlaceNode(name)
 
+// Read the comma delimited text file metros.csv into an RDD of Metro vertices
+// ignore lines that start with # 
+// and map the columns to: id, Metro(name, population)
 val metros: RDD[(VertexId, PlaceNode)] =
   sc.textFile("./EOADATA/metro.csv").
     filter(! _.startsWith("#")).
@@ -39,7 +46,7 @@ val countries: RDD[(VertexId, PlaceNode)] =
       (100L + row(0).toInt, Country(row(1)))
     }
 
-// import the edges
+/** import the edges */
 val mclinks: RDD[Edge[Int]] =
   sc.textFile("./EOADATA/metro_country.csv").
     filter(! _.startsWith("#")).
@@ -48,13 +55,18 @@ val mclinks: RDD[Edge[Int]] =
       Edge(0L + row(0).toInt, 100L + row(1).toInt, 1)
     }
 
-// create a graph
+/** create a graph */
+
+// concatenate the two sets of nodes into a single RDD
 val nodes = metros ++ countries
 val metrosGraph = Graph(nodes, mclinks)
 
 metrosGraph.vertices.take(5)
 metrosGraph.edges.take(5)
 
-// use Spark’s filter method to return vertices in the graph
+/** use Spark’s filter method to return vertices in the graph */
+
+// filter all of the edges in metrosGraph that have a source vertex Id of 1
+// and create a map of destination vertex Ids
 metrosGraph.edges.filter(_.srcId == 1).map(_.dstId).collect()
 metrosGraph.edges.filter(_.dstId == 103).map(_.srcId).collect()
